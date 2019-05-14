8eb64403ae3277bfdacc38f6545575e0fed8ade0
姬长信 <192666378@qq.com>
2019-05-14 19:03:07 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+App({
+    onLaunch: function() {},
+    onShow: function(n) {},
+    onHide: function() {},
+    onError: function(n) {},
+    globalData: {}
+});
+
+require("./utils/mtj-wx-sdk.js");
