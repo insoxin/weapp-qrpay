2ad8c694f265ea439672172c975904eebd1b1038
姬长信 <192666378@qq.com>
2019-05-14 19:09:37 +0800

    Add files via upload

---
+++ pages/其他/other.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        url: ""
+    },
+    onLoad: function(n) {
+        this.setData({
+            url: n.url
+        });
+    },
+    onReady: function() {},
+    onShow: function() {},
+    onHide: function() {},
+    onUnload: function() {},
+    onPullDownRefresh: function() {},
+    onReachBottom: function() {},
+    onShareAppMessage: function() {}
+});
